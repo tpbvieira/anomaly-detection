5b73eda1b9d46abf1f700ac56a70d2dbabf16d28
tpbvieira <tpbvieira@gmail.com>
2017-02-16 22:44:47 -0200

    some improvements for data generation and methods comparison

--- distributed-tensor-dictionary-learning/project.m
+++ distributed-tensor-dictionary-learning/project.m
@@ -12,6 +12,7 @@
 % methods
 %----------------------------------------------------------------------
 clc;
+clear all;
 set(0,'RecursionLimit',1200)
 mfile = 'project';
 %
@@ -19,42 +20,39 @@ s = 5;           % sparseness
 snr = 20;        % snr for added noise
 L = 2000;        % number of training vectors to use
 nofTrials = 5;   % at least so many trials should be done
-noIt = 100;      % number of iterations in each trial
-N = 80;
-K = 200;
-M1 = 10;
-M2 = 8;
-N1 = 20;
-N2 = 10;
+noIt = 300;      % number of iterations in each trial
+N = 20;
+K = 50;
+M1 = 5;
+M2 = 4;
+N1 = 10;
+N2 = 5;
 
 % select the methods to compare and define file names
-fileNameInfo = sprintf('%1i_%li_%li_%li_%li_%li',s,snr,L,nofTrials,noIt,N*K);
+fileNameInfo = sprintf('%1i_%li_%li_%li_%li',s,snr,L,N*K,noIt);
 fileNameSufix = sprintf('%s.mat',fileNameInfo);
 dataFiles = [
-             ['K', fileNameSufix] % 'K' = K-SVD,   
-             %['func_A', fileNameSufix] % 'A' = AK-SVD,
+             ['L', fileNameSufix] % 'L', 'Q', 'C', 'H' or 'E' = RLS-DLA (java),
              ['T', fileNameSufix] % 'T' = K-HOSVD
-             ['D', fileNameSufix] % 'D' = MOD,
-             ['O', fileNameSufix] % 'O' = T-MOD,
+             ['O', fileNameSufix] % 'O' = T-MOD,             
+             %['K', fileNameSufix] % 'K' = K-SVD,   
+             %['D', fileNameSufix] % 'D' = MOD,
+             %['A', fileNameSufix] % 'A' = AK-SVD,
              %['M', fileNameSufix] % 'M' = ILS-DLA MOD,             
              %['I', fileNameSufix] % 'I' = ILS-DLA MOD (java),             
-             %['B', fileNameSufix] % 'B' = RLS-DLA miniBatch
-             ['L', fileNameSufix] % 'L', 'Q', 'C', 'H' or 'E' = RLS-DLA (java),
+             %['B', fileNameSufix] % 'B' = RLS-DLA miniBatch             
              ];
 
 % plot configuration
-title({'Number of dictionary atoms identified per degrees.'; 'Elements: ';N*K});
-ylabel('Number of identified atoms.');
-xlabel('Required degrees for identification.');
-epsName = sprintf('%1i_%li_%li_%li_%li_%li.eps',s,snr,L,nofTrials,noIt,N*K);
-pngName = sprintf('%1i_%li_%li_%li_%li_%li.png',s,snr,L,nofTrials,noIt,N*K);
+epsName = sprintf('%1i_%li_%li_%li_%li.eps',s,snr,L,N*K,noIt);
+%pngName = sprintf('%1i_%li_%li_%li_%li.png',s,snr,L,N*K,noIt);
 colChar = 'brgmyck';
 colName = {'Blue', 'Red', 'Green', 'Magenta', 'Yellow', 'Cyan', 'Black'};
 degreesRates = [0.25:0.25:10, 10.5:0.5:25];
 clf; 
 hold on;
 grid on;
-x = 8; y = 7; yPadding = 10;
+x = 7; y = 2; yPadding = 2;
 
 % for selected methods for comparison
 for i=1:size(dataFiles,1);  
@@ -64,9 +62,9 @@ for i=1:size(dataFiles,1);
         fileName = dir(dataFiles(i,:));
         disp([mfile,': results of ',dataFiles(i,:),', ',fileName.date,'.']);
         load(dataFiles(i,:));                                               % load privious results
-        trialsDone = result.nofTrials;
-        if result.noIt ~= noIt
-            disp(['  ** OBS : noIt in file is ',int2str(result.noIt),...
+        trialsDone = results.nofTrials;
+        if results.noIt ~= noIt
+            disp(['  ** OBS : noIt in file is ',int2str(results.noIt),...
                   ' while wanted (here) noIt is ',int2str(noIt),' **.']);
         end
     else
@@ -76,42 +74,45 @@ for i=1:size(dataFiles,1);
     % execute trials for atoms identification
     if (nofTrials > trialsDone)
         method = dataFiles(i,1);
-        result = execDL(L, N, K, M1, M2, N1, N2, snr, method, s, noIt, nofTrials-trialsDone, 0)
+        results = execDL(L, N, K, M1, M2, N1, N2, snr, method, s, noIt, nofTrials-trialsDone, 0);
     end
     
     % prepare data
     yPos = zeros(size(K,1),1);
     for i1=1:numel(degreesRates);
-        yPos(i1) = sum(result.beta(:) < degreesRates(i1));
+        yPos(i1) = sum(results.beta(:) < degreesRates(i1));
     end
-    yPos = yPos/result.nofTrials; % simple mean by trials
+    yPos = yPos/results.nofTrials; % simple mean by trials
     
     % update plot for the current method
     plot(degreesRates, yPos, [colChar(i), '-']);
-    legend = text(x, y,['   noIt=', int2str(result.noIt),' and nofTrials=', int2str(result.nofTrials)]);
+    legend = text(x, y,['   noIt=', int2str(results.noIt),' and nofTrials=', int2str(results.nofTrials)]);
     set(legend, 'BackgroundColor', [1,1,1]); 
     y = y+yPadding;
-    I = find(yPos > y*result.nofTrials);
+    I = find(yPos > results.nofTrials);
     i1 = I(1);
     
     if ((i1>1) && (yPos(i1) > yPos(i1-1)))
-        xp = degreesRates(i1-1) + (degreesRates(i1)-degreesRates(i1-1))*(y*result.nofTrials - yPos(i1-1))/(yPos(i1)-yPos(i1-1));
+        xp = degreesRates(i1-1) + (degreesRates(i1)-degreesRates(i1-1))*(y*results.nofTrials - yPos(i1-1))/(yPos(i1)-yPos(i1-1));
     else
         xp = degreesRates(i1);
     end
     
-    legend = text(x, y,[colName{i},': ',result.method,' snr=',num2str(result.snr),' dB, L=',int2str(result.L)]);
+    legend = text(x, y,[colName{i},': ',results.method,' snr=',num2str(results.snr),' dB, L=',int2str(results.L)]);
     set(legend,'BackgroundColor',[1,1,1]);
     plot([xp,x-0.5], [y,y], [colChar(i),'.-']);
     y = y+yPadding;
 end
 
 % print plot
+title({'Number of dictionary atoms identified per degrees.'; 'Elements: ';N*K});
+ylabel('Number of identified atoms.');
+xlabel('Required degrees for identification.');
 legend = text(x, y,'Dictionary learning methods: ');
 set(legend,'BackgroundColor',[1,1,1]);
 print( gcf, '-depsc2', epsName );
 disp('Printed figure as: .eps');
-print( gcf, '-dpng', '-r80', pngName );
-disp('Printed figure as: .png');
+%print( gcf, '-dpng', '-r80', pngName );
+%disp('Printed figure as: .png');
 
 return
