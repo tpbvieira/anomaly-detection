41475ae204a46bc52ef78c7de4005cf7094df1a8
tpbvieira <tpbvieira@gmail.com>
2018-08-21 23:28:58 -0300

    eigensim function working. it is stil necessary to be able to deal with any size of X

--- mos-eigen-similarity/src/main/matlab/eigensim.m
+++ mos-eigen-similarity/src/main/matlab/eigensim.m
@@ -1,4 +1,4 @@
-function y = eigensim(X,w,l,cov,mos)
+function y = eigensim(X,w,l,cov,mos, th)
 % eigensim 
 %
 % SYNOPSIS: 
@@ -29,31 +29,33 @@ addpath('util/');
 nVar = size(X,1);
 nObs = size(X,2);
 numPeriods = l;
-periodsSize = w;
-threshold = 0.40;
+periodSize = w;
+threshold = th;
+y = zeros(1,nObs);
 
-% eigencovariance
-i = 1;
+% eigencovariance for each period to obtain the largest eigenvalues 
+X0Ini = 1;
+X0End = X0Ini + periodSize - 1; 
 maxEig = [];
 for li = 1:numPeriods
-    i2 = i+periodsSize-1; 
-    X0 = X(:, i:i2);
+    X0 = X(:, X0Ini:X0End);
 	if(strcmp(cov,'unit'))
         [Sx,Ex,Vx,Mx] = eigencorrelation(X0);
     elseif(strcmp(cov,'zmean'))
         [Sx,Ex,Vx,Mx] = eigencovariance(X0);
     end
-    i = i+periodsSize;
     maxEig(li) = Mx(1);
+    X0Ini = X0Ini + periodSize; 
+    X0End = X0Ini + periodSize - 1; 
 end
 
-% MOS
-numPCs = 0;
+% MOS from largest eigenvalues to predic the number of attacks
+nAttacks = 0;
 switch mos		
     case 'akaike'
-        numPCs = akaike_short2(maxEig,periodsSize);
+        nAttacks = akaike_short2(maxEig, periodSize);
     case 'edc'
-        numPCs = edc_short2(maxEig,periodsSize);
+        nAttacks = edc_short2(maxEig, periodSize);
     case 'eft'
         c = struct2cell(load([dataPath 'Pfa']));
         Pfa = c{1};
@@ -62,44 +64,41 @@ switch mos
         c = struct2cell(load([dataPath 'q']));
         waiting = c{1};
         getv_size = size(maxEig);
-        [eft_coeff,prob_found] = calc_coef_paper(getv_size(2),periodsSize,Pfa,coeff,waiting);        
-        numPCs = eft_short(maxEig,eft_coeff,getv_size(2),periodsSize);
+        [eft_coeff,prob_found] = calc_coef_paper(getv_size(2),periodSize,Pfa,coeff,waiting);        
+        nAttacks = eft_short(maxEig,eft_coeff,getv_size(2),periodSize);
     case 'radoi'
-        numPCs = ranoi_app(maxEig);
+        nAttacks = ranoi_app(maxEig);
     case 'mdl'
-        numPCs = mdl_short2(maxEig,periodsSize);
+        nAttacks = mdl_short2(maxEig,periodSize);
     case 'sure'
-        numPCs = sure_method(sureM,nVar,periodsSize);
+        nAttacks = sure_method(sureM,nVar,periodSize);
 end
 
-% gets index of periods pcs_q that are under attack
-pcs_q = NaN;
+% gets index of periods that are under attack
+attacked_q = NaN;
 maxEig_sorted = sort(maxEig,'descend');
-for i = 1:numPCs
-    pcs_q(i) = find(maxEig == maxEig_sorted(i));
+for i = 1:nAttacks
+    attacked_q(i) = find(maxEig == maxEig_sorted(i));
 end
 
-% for each period under attack, gets the times t with attack
-q_attacks = {};
-for pc = 1:numPCs
-    iRef = min(pcs_q) - 1;
-    i2 = iRef * periodsSize;
-    i1 = i2 - periodsSize + 1;
-    X0 = X(:, i1:i2);                                                        % the reference traffic without attack
-	if(strcmp(cov,'unit'))
-        [S0,E0,Vr0,Mr0] = eigencorrelation(X0);
-    elseif(strcmp(cov,'zmean'))
-        [S0,E0,Vr0,Mr0] = eigencovariance(X0);
-    end
+% for each period under attack, gets the times with attack
+iInit = 1;
+iEnd = periodSize;
+qRef = min(attacked_q) - 1;
+X0End = qRef * periodSize;
+X0Ini = X0End - periodSize + 1;
+X0 = X(:, X0Ini:X0End);                                                   % the reference traffic without attack
+if(strcmp(cov,'unit'))
+    [S0,E0,Vr0,Mr0] = eigencorrelation(X0);
+elseif(strcmp(cov,'zmean'))
+    [S0,E0,Vr0,Mr0] = eigencovariance(X0);
+end
+for iAtt = 1:nAttacks    
+    X0End = attacked_q(iAtt) * periodSize;
+    X0Ini = X0End - periodSize + 1;
     
-    i2 = pcs_q(pc) * periodsSize;
-    i1 = i2 - periodsSize + 1;
-    X1 = X(:, i1:i2);
-    
-    t_attacks = {};
-    
-    for t = 1:periodsSize
-        Xc = cat(2,X0,X1(:,1:t));
+    for t = X0Ini:X0End                                                   
+        Xc = cat(2, X0, X(:,X0Ini:t));                                    % incremental
         if (strcmp(cov,'unit'))
             [Sc,Ec,Vrc,Mrc] = eigencorrelation(Xc);
         else
@@ -107,28 +106,25 @@ for pc = 1:numPCs
         end
         cosTheta = dot(Vr0(:,Mr0(2)), Vrc(:,Mrc(2))) / (norm(Vr0(:,Mr0(2))) * norm(Vrc(:,Mrc(2))));
         cosTheta  = abs(cosTheta);
-        warning('Period(q) = %s, Time(t) = %s, cosTheta= %s',num2str(pcs_q(pc)),num2str(t),cosTheta);
+%         warning('Period(q) = %s, Time(t) = %s, cosTheta= %s',num2str(attacked_q(iAtt)),num2str(t),cosTheta);
         if(cosTheta < threshold)
-            t
-            X0 = cat(2,X0,X(:,1:t-1));
-            t_attacks = [t_attacks,t];                
-            t = t + 1;                
-            for a = t:periodsSize
+            y(1,t) = 1;                                                       % the time t is under attack and 
+            X0 = cat(2,X0, X(:,X0Ini:t-1));
+            tIni = t + 1;
+            for t = tIni:X0End                                              % individualized
                 if (strcmp(cov,'unit'))
-                    [S,E,Vrc,Mrc] = eigencorrelation(cat(2,X0,X(:,a)));
+                    [S,E,Vrc,Mrc] = eigencorrelation(cat(2, X0, X(:,t)));
                 else
-                    [S,E,Vrc,Mrc] = eigencovariance(cat(2,X0,X(:,a)));
+                    [S,E,Vrc,Mrc] = eigencovariance(cat(2, X0, X(:,t)));
                 end
                 cosTheta = dot(Vr0(:,Mr0(2)),Vrc(:,Mrc(2)))/(norm(Vr0(:,Mr0(2)))*norm(Vrc(:,Mrc(2))));
                 cosTheta  = abs(cosTheta);
-                warning('Period(q) = %s, Time(t) = %s, cosTheta= %s',num2str(pcs_q(pc)),num2str(a),cosTheta);                    
+%                 warning('Period(q) = %s, Time(t) = %s, cosTheta= %s',num2str(attacked_q(iAtt)),num2str(t),cosTheta);                    
                 if(cosTheta < threshold)
-                    a
-                    t_attacks = [t_attacks,a];
+                    y(1,t) = 1;                                               % the time t is under attack and 
                 end
             end
             break;
         end
     end    
-    q_attacks{pcs_q(pc)} = t_attacks
 end
--- mos-eigen-similarity/src/main/matlab/eigensimtest.m
+++ mos-eigen-similarity/src/main/matlab/eigensimtest.m
@@ -8,4 +8,4 @@ X5 = dlmread('/media/thiago/ubuntu/datasets/network/data/all/traffic/5.txt', '\t
 X6 = dlmread('/media/thiago/ubuntu/datasets/network/data/all/traffic/6.txt', '\t');
 X = [X1,X2,X3,X4,X5,X6];
 
-eigensim(X,20,6,'zmean','edc')
+eigensim(X,20,6,'zmean','edc', 0.40)
