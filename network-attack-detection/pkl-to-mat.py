c318089cf496daa5ffc0ea4f20e3d6c46785646b
tpbvieira <tpbvieira@gmail.com>
2018-10-29 22:33:49 -0200

    csv instead of mat

--- network-attack-detection/pkl-to-mat.py
+++ network-attack-detection/pkl-to-mat.py
@@ -1,37 +1,34 @@
 # coding=utf-8
-import scipy.io as sio
 import pandas as pd
 import os
 import gc
-import warnings
 import time
-warnings.filterwarnings(action='once')
 
+# track execution time
 start_time = time.time()
 
-# pickle files - have the same names but different directory
-pkl_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/pkl_fast/')
+# pickle files have the same names
+pkl_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/pkl/')
 pkl_directory = os.fsencode(pkl_path)
 pkl_files = os.listdir(pkl_directory)
-print("### Pickle Directory: ", pkl_directory)
+print("### Pkl Directory: ", pkl_directory)
+print("### Files: ", pkl_files)
 
-# mat files
-mat_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/mat_fast/')
-mat_directory = os.fsencode(mat_path)
+csv_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/raw')
+csv_directory = os.fsencode(csv_path)
 
-# for each file
 for sample_file in pkl_files:
-    # read pickle file
+
     pkl_file_path = os.path.join(pkl_directory, sample_file).decode('utf-8')
-    print('### Converting: ',pkl_file_path)
-    data_df = pd.read_pickle(pkl_file_path)
+    print('Loading: ', pkl_file_path)
+    pkl_df = pd.read_pickle(pkl_file_path)
+    print('Loaded: ', pkl_df.shape)
 
-    # save mat file
-    mat_file_path = os.path.join(mat_directory, sample_file).decode('utf-8')
-    data_dict = {'data_np': data_df.values}
-    sio.savemat(mat_file_path, data_dict)
+    csv_file_path = os.path.join(csv_directory, sample_file).decode('utf-8')
+    print('Saving: ', csv_file_path)
+    pkl_df.to_csv(csv_file_path)
+    print('Saved!')
 
-    # garbage collector
     gc.collect()
 
-print("--- Execution time: %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
