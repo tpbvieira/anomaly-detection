d0441fff3ec0293cd4a394cd1c217b73732660f4
tpbvieira <tpbvieira@gmail.com>
2018-11-02 12:47:18 -0200

    eigensim test for evaluation of f1_score, precision and recall

--- mos-eigen-similarity/src/test/matlab/eigensimCTU13Test.m
+++ mos-eigen-similarity/src/test/matlab/eigensimCTU13Test.m
@@ -2,12 +2,6 @@ clear;      % clear variables
 clc;        % clear console
 close all;  % close figures
 
-addpath('../../main/matlab/');
-addpath('../../main/matlab/eigen_analysis/');
-addpath('../../main/matlab/mos/');
-addpath('../../main/matlab/rsvd/rsvd');
-addpath('../../main/matlab/rsvd/rSVD-single-pass');
-
 % column_types = {
 %     'StartTime': 'str',
 %     'Dur': 'single',
@@ -24,64 +18,86 @@ addpath('../../main/matlab/rsvd/rSVD-single-pass');
 %     'TotBytes': 'uint32',
 %     'SrcBytes': 'uint32',
 %     'Label': 'uint8'}
-
 % drop_features = {
 %     'drop_features01': ['SrcAddr', 'DstAddr', 'sTos', 'Sport', 'SrcBytes', 'TotBytes', 'Proto'],
 %     'drop_features02': ['SrcAddr', 'DstAddr', 'sTos', 'Sport', 'SrcBytes', 'TotBytes'],
 %     'drop_features03': ['SrcAddr', 'DstAddr', 'sTos', 'Sport', 'SrcBytes', 'Proto'],
 %     'drop_features04': ['SrcAddr', 'DstAddr', 'sTos', 'Proto']
 % }
-
-disp('### Loading...')
-filePath = '/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/csv/capture20110818-2.csv';
 % opts = detectImportOptions(filePath);
 % opts = setvartype(opts,{'Dur'},'single');
 % opts = setvartype(opts,{'TotBytes','SrcBytes'},'uint32');
 % opts = setvartype(opts,{'Sport','Dport','TotPkts'},'uint16');
 % opts = setvartype(opts,{'Proto','Dir','State','sTos','dTos','Label'},'uint8');
 % opts = setvartype(opts,{'StartTime','SrcAddr','DstAddr'},'string');
-X = readtable(filePath);
-X.SrcAddr = [];
-X.DstAddr = [];
-X.sTos = [];
-X.Proto = [];
 
-X = sortrows(X);
-y = X.Label;
+addpath('../../main/matlab/');
+addpath('../../main/matlab/eigen_analysis/');
+addpath('../../main/matlab/mos/');
+addpath('../../main/matlab/rsvd/rsvd');
+addpath('../../main/matlab/rsvd/rSVD-single-pass');
 
-X.Label = [];
-X.StartTime = [];
-X = table2array(X);
+resultsFolder = '/home/thiago/dev/projects/discriminative-sensing/mos-eigen-similarity/src/test/matlab/results/';
+fileFolder = '/media/thiago/ubuntu/datasets/network/stratosphere-botnet-2011/ctu-13/csv/';
+fileList = dir(fileFolder);
 
-X = X';
-X = single(X);
+for i = 3:size(fileList,1)
+    
+    % source
+    fileName = fileList(i).name;
+    filePath = strcat(fileFolder,fileName);
+    
+    % destination
+    resFolder = strcat(resultsFolder,fileName(1:end-4),'/');
+    
+    disp('### Loading...')
+    X = readtable(filePath);
+    % Select data and drop features for features01 set
+    X.SrcAddr = [];
+    X.DstAddr = [];
+    X.sTos = [];
+    X.Proto = [];
 
+    X = sortrows(X);
+    y = X.Label;
+    testFile = strcat(resFolder,'y_test.csv');
+    if exist(testFile, 'file') ~= 2
+        dlmwrite(testFile,y,'delimiter','\t');
+        fprintf('### Saved: %s\n',testFile)
+    end
+    X.Label = [];
+    X.StartTime = [];
+    X = table2array(X);
+    X = X';
 
-windowSizes = [20 40 80 160 300];
-windows = [6 10 15 20 40];
-% mos = {'akaike','edc','eft','radoi','sure'};
-mos = {'akaike','edc'};
-ths = [0.1 0.2 0.3 0.4 0.5];
+    windowSizes = [20 40 80 160 300];
+    windows = [6 10 15 20 40];
+    % mos = {'akaike','edc','eft','radoi','sure'};
+    mos = {'akaike','edc'};
+    ths = [0.1 0.2 0.3 0.4 0.5];
 
-for wsz = windowSizes
-    for ws = windows
-        for m = mos
-            for th = ths
-                unitStr = sprintf('results/%d_%d_unit_eig_%s_%0.1f.csv',wsz,ws,m{1},th);
-                if exist(unitStr, 'file') ~= 2
-                    fprintf('### Test Scenario features01 - %s\n',unitStr)
-                    tic;
-                    yTest = eigensim(X,wsz,ws,'unit','eig',m{1},th);
-                    dlmwrite(unitStr,yTest','delimiter','\t');
-                    toc;
-                end
-                zmeanStr = sprintf('results/%d_%d_zmean_eig_%s_%0.1f.csv',wsz,ws,m{1},th);               
-                if exist(zmeanStr, 'file') ~= 2
-                    fprintf('### Test Scenario features01 - %s\n',zmeanStr)
-                    tic;
-                    yTest = eigensim(X,wsz,ws,'zmean','eig',m{1},th);
-                    dlmwrite(zmeanStr,yTest','delimiter','\t');
-                    toc;
+    for wsz = windowSizes
+        for ws = windows
+            for m = mos
+                for th = ths
+                    unitStr = sprintf('%d_%d_unit_eig_%s_%0.1f.csv',wsz,ws,m{1},th);
+                    destFile = strcat(resFolder, unitStr);
+                    if exist(destFile, 'file') ~= 2
+                        fprintf('### Test Scenario: features01 - %s\n',unitStr)
+                        tic;
+                        yTest = eigensim(X,wsz,ws,'unit','eig',m{1},th);
+                        dlmwrite(unitStr,yTest','delimiter','\t');
+                        toc;
+                    end
+                    zmeanStr = sprintf('%d_%d_zmean_eig_%s_%0.1f.csv',wsz,ws,m{1},th);
+                    destFile = strcat(resFolder, zmeanStr);
+                    if exist(destFile, 'file') ~= 2
+                        fprintf('### Test Scenario: features01 - %s\n',zmeanStr)
+                        tic;
+                        yTest = eigensim(X,wsz,ws,'zmean','eig',m{1},th);
+                        dlmwrite(zmeanStr,yTest','delimiter','\t');
+                        toc;
+                    end
                 end
             end
         end
--- mos-eigen-similarity/src/test/python/eigensimF1ScoreTest.py
+++ mos-eigen-similarity/src/test/python/eigensimF1ScoreTest.py
@@ -1,10 +1,37 @@
 import pandas as pd
-from sklearn.metrics import classification_report, average_precision_score, f1_score, recall_score, precision_score
+import os
+import gc
+from sklearn.metrics import f1_score, recall_score, precision_score
 
-y_test = pd.read_csv('/home/thiago/dev/projects/discriminative-sensing/mos-eigen-similarity/src/test/matlab/train_label.csv', header=None)
-print(y_test.shape)
-y_predic = pd.read_csv('/home/thiago/dev/projects/discriminative-sensing/mos-eigen-similarity/src/test/matlab/20_6_unit_eig_akaike_0.1.csv')
-print(y_predic.shape)
 
-f = f1_score(y_test, y_predic, average = "binary")
-print("### F1Score: ", f)
+def get_classification_report(m_y_test, m_y_predic):
+    m_f1 = f1_score(m_y_test, m_y_predic, average="binary")
+    m_recall = recall_score(m_y_test, m_y_predic, average="binary")
+    m_precision = precision_score(m_y_test, m_y_predic, average="binary")
+    return m_f1, m_recall, m_precision
+
+
+results_path = os.path.join('/home/thiago/dev/projects/discriminative-sensing/mos-eigen-similarity/src/test/matlab/results/')
+results_dir = os.fsencode(results_path)
+results_dirs = os.listdir(results_dir)
+
+for res_dir in results_dirs:
+
+    res_dir_path = os.path.join(results_dir, res_dir).decode('utf-8')
+    res_directory = os.fsencode(res_dir_path)
+    res_files = os.listdir(res_directory)
+
+    y_test = pd.read_csv(res_dir_path + '/y_test.csv')
+
+    for res_file in res_files:
+
+        if res_file != 'y_test.csv'.encode('utf-8'):
+
+            res_file_path = os.path.join(res_directory, res_file).decode('utf-8')
+
+            if os.path.isfile(res_file_path):
+
+                y_predict = pd.read_csv(res_file_path)
+                f1, recall, precision = get_classification_report(y_test, y_predict)
+                print('###[Eigensim][', res_file, '] Test. F1:', f1, ', Recall:', recall, ', Precision:', precision)
+                gc.collect()
