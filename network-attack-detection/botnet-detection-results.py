0bbd61a81d5d5c545b1199ce3ba7ea931a155210
tpbvieira <tpbvieira@gmail.com>
2019-01-20 18:00:10 -0200

    sort boxplot result by window and algorithm

--- network-attack-detection/botnet-detection-results.py
+++ network-attack-detection/botnet-detection-results.py
@@ -6,12 +6,12 @@ import seaborn as sns
 warnings.filterwarnings("ignore")
 
 # result file path
-pkl_path = os.path.join('/home/thiago/Downloads/results/21/')
+pkl_path = os.path.join('/home/thiago/Downloads/results/20/')
 pkl_directory = os.fsencode(pkl_path)
 file_list = os.listdir(pkl_directory)
 
 # initialize the dataframe
-columns = ['F1','method', 'scenarios']
+columns = ['F1','methods', 'algs','windows', 'scenarios']
 result_df = pd.DataFrame(columns=columns)
 
 # for each file/case
@@ -19,9 +19,11 @@ for sample_file in file_list:
     # split the file name into variables
     file_name = sample_file.decode('utf-8')
     tokens = file_name.split('_')
+    alg = tokens[0]
     it = tokens[1]
+    window = tokens[2]
     scenario = tokens[3]
-    method = "%s_%s" % (tokens[0], tokens[2])
+    method = "%s_%s" % (alg, window)
 
     # read the file
     pkl_file_path = os.path.join(pkl_directory, sample_file).decode('utf-8')
@@ -31,10 +33,12 @@ for sample_file in file_list:
     # get F1 and variables from the file name
     m_f1 = df.values
     methods = [method] * len(m_f1)
-    scenarios = [tokens[3]] * len(m_f1)
+    algs = [alg] * len(m_f1)
+    windows = [window] * len(m_f1)
+    scenarios = [scenario] * len(m_f1)
 
     # generate a new dataframe with F1 and variables from the file name
-    values_list = list(zip(m_f1, methods, scenarios))
+    values_list = list(zip(m_f1, methods, algs, windows, scenarios))
     df = pd.DataFrame(values_list,columns= columns)
 
     # append into result file
@@ -43,9 +47,10 @@ for sample_file in file_list:
 m_scenarios = result_df.scenarios.unique()
 
 for m_scenario in m_scenarios:
-    fig_name = "%s.png" % (m_scenario)
     result_scenario_df = result_df.loc[result_df['scenarios'] == m_scenario]
-    sns.boxplot(x="method", y="F1", data=result_scenario_df, palette="PRGn")
+    result_scenario_df = result_scenario_df.sort_values(['windows', 'algs'], ascending=[True, False])
+
+    sns.boxplot(x="methods", y="F1", data=result_scenario_df, palette="PRGn")
+    fig_name = "%s.png" % m_scenario
     plt.savefig(fig_name)
     plt.close()
-    # break
