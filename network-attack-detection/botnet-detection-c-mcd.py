24492f36b3ed0606b862af951bcdb564a2420a98
thiago <tpbvieira@gmail.com>
2019-03-10 16:35:16 -0300

    after test chi2 and dont get good results

--- network-attack-detection/botnet-detection-c-mcd.py
+++ network-attack-detection/botnet-detection-c-mcd.py
@@ -2,6 +2,7 @@
 import os, gc, time, warnings, pickle
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 from sklearn.metrics import f1_score, recall_score, precision_score
 from sklearn.utils import shuffle
 from k_mcd_outlier_detection import MEllipticEnvelope
@@ -14,6 +15,7 @@ def semiSupervisedCV(t_normal_df, t_cv_df, t_cv_label, n_it):
     m_best_model = MEllipticEnvelope()
     m_best_contamination = -1
     m_best_f1 = -1
+    m_best_alg = ''
 
     # suffle cv data
     t_cv_df['Labels'] = t_cv_label
@@ -38,6 +40,7 @@ def semiSupervisedCV(t_normal_df, t_cv_df, t_cv_label, n_it):
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = mcd_f1
+                m_best_alg = 'mcd'
 
             # kurtosis prediction
             k_pred = m_ell_model.kurtosis_prediction(t_cv_df)
@@ -46,6 +49,7 @@ def semiSupervisedCV(t_normal_df, t_cv_df, t_cv_label, n_it):
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = k_f1
+                m_best_alg = 'k-mcd'
 
             # skewness prediction
             s_pred = m_ell_model.skewness_prediction(t_cv_df)
@@ -54,8 +58,9 @@ def semiSupervisedCV(t_normal_df, t_cv_df, t_cv_label, n_it):
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = s_f1
+                m_best_alg = 's-mcd'
 
-    return m_best_model, m_best_contamination, m_best_f1
+    return m_best_model, m_best_contamination, m_best_f1, m_best_alg
 
 
 def unsupervisedCV(t_cv_df, t_cv_label, n_it):
@@ -63,6 +68,7 @@ def unsupervisedCV(t_cv_df, t_cv_label, n_it):
     m_best_model = MEllipticEnvelope()
     m_best_contamination = -1
     m_best_f1 = -1
+    m_best_alg = ''
 
     # suffle cv data
     t_cv_df['Labels'] = t_cv_label
@@ -87,43 +93,48 @@ def unsupervisedCV(t_cv_df, t_cv_label, n_it):
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = mcd_f1
+                m_best_alg = 'mcd'
+                print(m_best_alg, m_best_f1)
 
             # kurtosis prediction
-            k_pred = m_ell_model.kurtosis_prediction(t_cv_df)
-            k_f1 = f1_score(m_cv_label, k_pred, average="binary")
+            k_pred_label = m_ell_model.kurtosis_prediction(t_cv_df)
+            k_f1 = f1_score(m_cv_label, k_pred_label, average="binary")
             if k_f1 > m_best_f1:
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = k_f1
+                m_best_alg = 'k-mcd'
+                print(m_best_alg , m_best_f1)
 
             # skewness prediction
-            s_pred = m_ell_model.skewness_prediction(t_cv_df)
-            s_f1 = f1_score(m_cv_label, s_pred, average="binary")
+            s_pred_label = m_ell_model.skewness_prediction(t_cv_df)
+            s_f1 = f1_score(m_cv_label, s_pred_label, average="binary")
             if s_f1 > m_best_f1:
                 m_best_model = m_ell_model
                 m_best_contamination = m_contamination
                 m_best_f1 = s_f1
+                m_best_alg = 's-mcd'
+                print(m_best_alg, m_best_f1)
 
-    return m_best_model, m_best_contamination, m_best_f1
+    return m_best_model, m_best_contamination, m_best_f1, m_best_alg
 
 
-start_time = time.time()
-
-raw_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere_botnet_2011/ctu_13/pkl_sum/')
+raw_path = os.path.join('data/ctu_13/pkl_sum_fast/')
 raw_directory = os.fsencode(raw_path)
-
-pkl_path = os.path.join('/media/thiago/ubuntu/datasets/network/stratosphere_botnet_2011/ctu_13/pkl_sum/')
+pkl_path = os.path.join('data/ctu_13/pkl_sum_fast/')
 pkl_directory = os.fsencode(pkl_path)
 file_list = os.listdir(pkl_directory)
-
 it = 10
 
+start_time = time.time()
 # for each feature set
 for features_key, value in drop_agg_features.items():
 
     # for each file/case
     for sample_file in file_list:
+
         result_file = "results/pkl_sum_dict/%d/data/c-mcd_%d_%s" % (it, it, sample_file.decode('utf-8'))
+
         if not os.path.isfile(result_file):
 
             # read pickle file with pandas or...
@@ -140,47 +151,50 @@ for features_key, value in drop_agg_features.items():
             gc.collect()
 
             # data splitting
-            # norm_train_df, cv_df, test_df, cv_label, test_label = data_splitting(df, drop_agg_features[features_key])
-            train_df, train_label_df, test_df, test_label_df = unsupervised_data_splitting(df, drop_agg_features[features_key])
+            norm_train_df, cv_df, test_df, cv_label_df, test_label_df = data_splitting(df, drop_agg_features[features_key])
+            # train_df, train_label_df, test_df, test_label_df = unsupervised_data_splitting(df, drop_agg_features[features_key])
 
             c_mcd_result_dict = {}
             for i in range(it):
+
                 # Cross-Validation and model selection
-                # ell_model, best_contamination, best_f1 = semiSupervisedCV(norm_train_df, cv_df, cv_label, 1)
-                ell_model, best_contamination, best_f1 = unsupervisedCV(train_df, train_label_df, 1)
-                print('###[c-mcd][', features_key, '] Cross-Validation. Contamination:', best_contamination,', F1:', best_f1)
+                train_best_model, train_best_cont, train_best_f1, train_best_alg = semiSupervisedCV(norm_train_df, cv_df, cv_label_df, 1)
+
+                # train_best_model, train_best_cont, train_best_f1, train_best_alg = unsupervisedCV(train_df, train_label_df, 1)
+                print('###[c-mcd][', features_key, '] Cross-Validation. Contamination:', train_best_cont,', F1:', train_best_f1,', Alg:', train_best_alg)
 
                 test_label = test_label_df.astype(np.int8)
 
                 # MCD Prediction Test
-                mcd_pred_label = ell_model.mcd_prediction(test_df)
-                mcd_pred_dist_ = ell_model.prediction_dist_
+                mcd_pred_label = train_best_model.mcd_prediction(test_df)
+                mcd_pred_dist_ = train_best_model.prediction_dist_
                 mcd_pred_label[mcd_pred_label == 1] = 0
                 mcd_pred_label[mcd_pred_label == -1] = 1
                 t_f1, t_Recall, t_Precision = get_classification_report(test_label, mcd_pred_label)
                 print('###[mcd][', features_key, '] Test. F1:', t_f1, ', Recall:', t_Recall, ', Precision:', t_Precision)
 
                 # K-MCD Prediction Test
-                k_pred_label = ell_model.kurtosis_prediction(test_df)
-                k_pred_dist_ = ell_model.prediction_dist_
+                k_pred_label = train_best_model.kurtosis_prediction(test_df)
+                k_pred_dist_ = train_best_model.prediction_dist_
                 t_f1, t_Recall, t_Precision = get_classification_report(test_label, k_pred_label)
                 print('###[k-mcd][', features_key, '] Test. F1:', t_f1, ', Recall:', t_Recall, ', Precision:', t_Precision)
 
                 # S-MCD Prediction Test
-                s_pred_label = ell_model.skewness_prediction(test_df)
-                s_pred_dist_ = ell_model.prediction_dist_
+                s_pred_label = train_best_model.skewness_prediction(test_df)
+                s_pred_dist_ = train_best_model.prediction_dist_
                 t_f1, t_Recall, t_Precision = get_classification_report(test_label, s_pred_label)
                 print('###[s-mcd][', features_key, '] Test. F1:', t_f1, ', Recall:', t_Recall, ', Precision:', t_Precision)
 
                 c_mcd_pred = {
-                    "best_contamination": best_contamination,
-                    "train_f1": best_f1,
-                    "mcd_pred_dist_": mcd_pred_dist_,
-                    "k_pred_dist_": k_pred_dist_,
-                    "s_pred_dist_": s_pred_dist_,
-                    "mcd_pred_label": mcd_pred_label,
-                    "k_pred_label": k_pred_label,
-                    "s_pred_label": s_pred_label,
+                    "train_best_cont": train_best_cont,
+                    "train_best_f1": train_best_f1,
+                    "train_best_alg": train_best_alg,
+                    "test_mcd_dist_": mcd_pred_dist_,
+                    "test_k_dist_": k_pred_dist_,
+                    "test_s_dist_": s_pred_dist_,
+                    "test_mcd_label": mcd_pred_label,
+                    "test_k_label": k_pred_label,
+                    "test_s_label": s_pred_label,
                     "test_label": test_label
                 }
                 c_mcd_result_dict[i] = c_mcd_pred
